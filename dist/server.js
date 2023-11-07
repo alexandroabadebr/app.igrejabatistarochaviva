d6cc64f16030a63cf17ef49c7c3385554ded5326
Alexandro Abade <alexandroabade@gmail.com>
2023-11-06 22:03:35 -0300

    config server.js

--- dist/server.js
+++ dist/server.js
@@ -1,1 +1,21 @@
-"use strict";
+//"use strict";
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Servir arquivos estáticos na pasta 'dist'
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Rota para servir o arquivo 'index.html'
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+// Iniciar o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
